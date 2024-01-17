a07aaf6823378ab87af64a1228b8077f3e34987e
Krittidet Satham <kittidet.sathum22@gmail.com>
2024-01-17 13:39:23 +0700

    comment @Profile("db")

--- src/main/java/camt/demo/dao/EventDaoDbImpl.java
+++ src/main/java/camt/demo/dao/EventDaoDbImpl.java
@@ -9,7 +9,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Profile("db")
+//@Profile("db")
 @Repository
 @RequiredArgsConstructor
 public class EventDaoDbImpl implements EventDao {
